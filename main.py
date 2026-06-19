315f48223df1e27540996883a0b76a4fca4cd071
Aylar <aylar.hosseinzadeh03@gmail.com>
2026-06-19 16:43:47 +0330

    feat: implement github url parser with owner and repo extraction

--- main.py
+++ main.py
@@ -6,13 +6,42 @@ print("========================= \nGitHub Repository Analyzer\n=================
 def validate_url(url):
     return url.startswith("https://github.com/")
       
+
+def normalize_url(url):
+     if url.endswith("/"):
+        return url[:-1]
+     return url
+
+def extract_owner_repo(url):
+    parts = url.split("/")
+    owner = parts[3]
+    repo = parts[4]
+    return owner , repo
+
+
 url = input("ENTER URL: ")
 
-if validate_url(url):
-    print("valid")
-else:
-    print("invalid")
+if not validate_url(url):
+    print("Invalid GitHub URL.")
+    exit()
 
+owner , repo = extract_owner_repo(url)
+print(f"owner: {owner} and repo: {repo}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
 
