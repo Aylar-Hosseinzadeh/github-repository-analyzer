7008dd2217655236f50372cd85c186161ab6a6d4
Aylar Hosseinzadeh <aylar.hosseinzadeh@gamil.com>
2026-06-30 20:06:40 +0330

    feat(report): display additional repository details

--- report.py
+++ report.py
@@ -5,10 +5,24 @@ def show_repository_report(data):
     stars = data["stargazers_count"]
     forks = data["forks_count"]
     language = data["language"]
+    description = data["description"]
+    open_issues = data["open_issues_count"]
+    default_branch = data["default_branch"]
+
+    if data["license"] :
+        license_name = data["license"]["name"]
+    else:
+        license_name = "No license"
+
     print(f"name : {name}")
+    print(f"description : {description}")
+    print(f"language : {language}")
     print(f"stars : {stars}")
     print(f"forks : {forks}")
-    print(f"language : {language}")
+    print(f"open issues : {open_issues}")
+    print(f"default branch : {default_branch}")
+    print(f"license : {license_name}")
+
 
     
 
